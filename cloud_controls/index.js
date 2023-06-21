a69822644cabe727dc548ae83976fb63c5597a29
Karsten Buckstegge <k.buckstegge@gmail.com>
2023-06-21 16:16:38 +0200

    add arduino sketch and update code and packages

--- cloud_controls/index.js
+++ cloud_controls/index.js
@@ -1,153 +1,156 @@
-const containerElement = document.querySelector('.container');
-const cloud = document.querySelector('#character-cloud');
-const controls = document.querySelectorAll('.control');
+const containerElement = document.querySelector(".container");
+const cloud = document.querySelector("#character-cloud");
+const controls = document.querySelectorAll(".control");
 
-const outlines = document.querySelectorAll('#character-cloud .outline');
-const blockColors = document.querySelectorAll('#character-cloud .c');
-const allCharsSelector = document.querySelectorAll('#character-cloud .character');
+const outlines = document.querySelectorAll("#character-cloud .outline");
+const blockColors = document.querySelectorAll("#character-cloud .c");
+const allCharsSelector = document.querySelectorAll(
+  "#character-cloud .character"
+);
 
 const allCharacters = [
-    'AlienFish',
-    'Snake',
-    'Mouse',
-    'MiniShark',
-    'Birdman',
-    'Waspman',
-    'HappySnake',
-    'ConfusedFish',
-    'Mexican',
-    'Jet',
-    'Worm',
-    'HappyShark',
-    'BigFish',
-    'FatHead',
-    'Bird',
-    'Octopus',
-    'Octopus_Bubble',
-    'Ghost',
-    'ScaredMouse',
-    'UFO',
-    'Rubberhose',
-    'Whale',
-    'Ray',
-    'Mantanee',
-    'Airplane',
-    'Seagull',
-    'Maggot'
-]
+  "AlienFish",
+  "Snake",
+  "Mouse",
+  "MiniShark",
+  "Birdman",
+  "Waspman",
+  "HappySnake",
+  "ConfusedFish",
+  "Mexican",
+  "Jet",
+  "Worm",
+  "HappyShark",
+  "BigFish",
+  "FatHead",
+  "Bird",
+  "Octopus",
+  "Octopus_Bubble",
+  "Ghost",
+  "ScaredMouse",
+  "UFO",
+  "Rubberhose",
+  "Whale",
+  "Ray",
+  "Mantanee",
+  "Airplane",
+  "Seagull",
+  "Maggot",
+];
 
 const allCharacterElements = [];
 
 let animationSpeed = 2;
-const animationSpeedStep = .6;
+const animationSpeedStep = 0.6;
 
 let hideCharacters = false;
 let hideCharLoop;
 
-const setAnimationSpeed = animationInt => {
-    let animationSpeedTemp = 0;
-    
-    if (animationInt === 1) {
-        animationSpeedTemp = animationSpeed + animationSpeedStep;
-    } else {
-        animationSpeedTemp = animationSpeed - animationSpeedStep;
-    }
+const setAnimationSpeed = (animationInt) => {
+  let animationSpeedTemp = 0;
 
-    if (animationSpeedTemp > 0.1 && animationSpeedTemp < 5.1) {
-        animationSpeed = animationSpeedTemp;
-    }
+  if (animationInt === 1) {
+    animationSpeedTemp = animationSpeed + animationSpeedStep;
+  } else {
+    animationSpeedTemp = animationSpeed - animationSpeedStep;
+  }
 
-    cloud.style.animationDuration = `${animationSpeed}s`;
-    speedChangeCharHide();
-}
+  if (animationSpeedTemp > 0.1 && animationSpeedTemp < 5.1) {
+    animationSpeed = animationSpeedTemp;
+  }
 
-const getRandomCharacter = () => {
-    const i = Math.max(0, Math.round(Math.random() * allCharacters.length - 1));
-    return i;
-}
-
-const hideCharacter = () => {
-    const charId = getRandomCharacter();
-
-    const char = allCharacterElements[charId];
-    char.style.opacity = 0;
-
-    setTimeout(() => {
-        char.style.opacity = 1;
-    }, 2500);
-}
-
-const hideCharactersLoop = () => {
-    if (!hideCharacters) {
-        hideCharLoop = setInterval(hideCharacter, animationSpeed * 500);
-    } else {
-        clearInterval(hideCharLoop);
-    }
-
-    hideCharacters = !hideCharacters;
-}
-
-const speedChangeCharHide = () => {
-    if (!hideCharacters) { return; };
-
-    clearInterval(hideCharLoop);
-    hideCharLoop = setInterval(hideCharacter, animationSpeed * 500);
-}
-
-const queryAllCharacterElements = () => {
-    allCharacters.forEach(charName => {
-        const charElement = document.querySelector(`.${charName}`);
-        allCharacterElements.push(charElement);
-    });
+  cloud.style.animationDuration = `${animationSpeed}s`;
+  speedChangeCharHide();
 };
 
-const runAnimation = animationInt => {
-    if (animationInt <= 2) {
-        setAnimationSpeed(animationInt);
-    } else if (animationInt === 8) {
-        hideCharactersLoop();
-    } else {
-        containerElement.classList.toggle(`animation--${animationInt}`);
-    }
-}
+const getRandomCharacter = () => {
+  const i = Math.max(0, Math.round(Math.random() * allCharacters.length - 1));
+  return i;
+};
 
-const newPhoneInput = input => {
-    console.log('input:', input);
+const hideCharacter = () => {
+  const charId = getRandomCharacter();
 
-    const numInput = parseInt(input);
+  const char = allCharacterElements[charId];
+  char.style.opacity = 0;
 
-    if (input === 'hung up' || input === 'picked up') {
-        console.log('input');
-    } else if (numInput > 0 && numInput < 9) {
-        runAnimation(numInput);
-    } else {
-        console.error(`unknown input ${input}`);
-    }
-}
+  setTimeout(() => {
+    char.style.opacity = 1;
+  }, 2500);
+};
+
+const hideCharactersLoop = () => {
+  if (!hideCharacters) {
+    hideCharLoop = setInterval(hideCharacter, animationSpeed * 500);
+  } else {
+    clearInterval(hideCharLoop);
+  }
+
+  hideCharacters = !hideCharacters;
+};
+
+const speedChangeCharHide = () => {
+  if (!hideCharacters) {
+    return;
+  }
+
+  clearInterval(hideCharLoop);
+  hideCharLoop = setInterval(hideCharacter, animationSpeed * 500);
+};
+
+const queryAllCharacterElements = () => {
+  allCharacters.forEach((charName) => {
+    const charElement = document.querySelector(`.${charName}`);
+    allCharacterElements.push(charElement);
+  });
+};
+
+const runAnimation = (animationInt) => {
+  if (animationInt <= 2) {
+    setAnimationSpeed(animationInt);
+  } else if (animationInt === 8) {
+    hideCharactersLoop();
+  } else {
+    containerElement.classList.toggle(`animation--${animationInt}`);
+  }
+};
+
+const newPhoneInput = (input) => {
+  console.log("input:", input);
+
+  const numInput = parseInt(input);
+
+  if (input.includes("hungUp") || input.includes("lifted")) {
+    console.log("input");
+  } else if (numInput > 0 && numInput < 9) {
+    runAnimation(numInput);
+  } else {
+    console.error(`unknown input ${input}`);
+  }
+};
 
 const connectToWebsocket = () => {
-    socket = new WebSocket('ws://localhost:3000');
+  socket = new WebSocket("ws://localhost:3000");
 
-    socket.onopen = () => console.log('connected to', socket);
-    socket.onerror = err => console.error('WebSocket error', err);
+  socket.onopen = () => console.log("connected to", socket);
+  socket.onerror = (err) => console.error("WebSocket error", err);
 
-    socket.onmessage = message => newPhoneInput(message.data);
-}
+  socket.onmessage = (message) => newPhoneInput(message.data);
+};
 
 const init = () => {
-    cloud.style.animationDuration = `${animationSpeed}s`;
+  cloud.style.animationDuration = `${animationSpeed}s`;
 
-    [...controls].forEach((control) => {
-        control.addEventListener('click', (e) => {
-            const animationInt = parseInt(e.currentTarget.innerHTML);
-            
-            runAnimation(animationInt); 
-        })
+  [...controls].forEach((control) => {
+    control.addEventListener("click", (e) => {
+      const animationInt = parseInt(e.currentTarget.innerHTML);
+
+      runAnimation(animationInt);
     });
-    queryAllCharacterElements();
+  });
+  queryAllCharacterElements();
 
-    connectToWebsocket();
-}
-
+  connectToWebsocket();
+};
 
 init();
--- cloud_controls/phone.js
+++ cloud_controls/phone.js
@@ -1,22 +1,21 @@
-const WebSocket = require('ws');
-const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
+const WebSocket = require("ws");
+const { SerialPort } = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
 
 const wss = new WebSocket.Server({ port: 3000 });
-
-const path = '/dev/tty.usbmodem14201';
+const path = "/dev/cu.usbmodem144101";
 
 // start server
-wss.on('connection', (ws) => {
-    console.log('Client connected to WebSocket');
+wss.on("connection", (ws) => {
+  console.log("Client connected to WebSocket");
 
-    // read serial input
-    const serial = new SerialPort(path, { baudRate: 115200 })
-    const parser = new Readline()
-    serial.pipe(parser)
-    
-    parser.on('data', line => {
-        console.log(`Serial Data recieved: ${line}`);
-        ws.send(line);
-    });
+  // read serial input
+  const serial = new SerialPort({ path, baudRate: 115200 });
+  const parser = new ReadlineParser();
+  serial.pipe(parser);
+
+  parser.on("data", (line) => {
+    console.log(`Serial Data recieved: ${line}`);
+    ws.send(line);
+  });
 });
